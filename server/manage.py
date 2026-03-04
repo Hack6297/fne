1afb2071f80be73dcb62a1d496525a89646b65c1
Hack6297 <164305482+Hack6297@users.noreply.github.com>
2026-03-03 21:09:53 -0800

    Update manage.py

--- server/manage.py
+++ server/manage.py
@@ -1,4 +1,3 @@
-@"
 #!/usr/bin/env python
 import os
 import sys
@@ -7,4 +6,3 @@ if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fne_backend.settings')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
-"@ | Out-File -Encoding utf8 server/manage.py
