ca040ac8f2d625444b3c3a1d9abc2e777cc66234
Hack6297 <164305482+Hack6297@users.noreply.github.com>
2026-03-03 21:16:46 -0800

    Refactor project_list and project_detail responses

--- server/community/views.py
+++ server/community/views.py
@@ -1,4 +1,3 @@
-@"
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
@@ -11,14 +10,31 @@ def index(request):
 @csrf_exempt
 def project_list(request):
     projects = Project.objects.all()[:50]
-    data = [{'id': p.id, 'title': p.title, 'author': p.author, 'description': p.description, 'likes': p.likes, 'plays': p.plays, 'created_at': str(p.created_at), 'thumbnail': p.thumbnail} for p in projects]
+    data = []
+    for p in projects:
+        data.append({
+            'id': p.id,
+            'title': p.title,
+            'author': p.author,
+            'description': p.description,
+            'likes': p.likes,
+            'plays': p.plays,
+            'created_at': str(p.created_at),
+            'thumbnail': p.thumbnail
+        })
     return JsonResponse({'projects': data})
 
 @csrf_exempt
 def project_create(request):
     if request.method == 'POST':
         body = json.loads(request.body)
-        p = Project.objects.create(title=body.get('title', 'Untitled'), author=body.get('author', 'Anonymous'), description=body.get('description', ''), level_data=body.get('level_data', {}), thumbnail=body.get('thumbnail', ''))
+        p = Project.objects.create(
+            title=body.get('title', 'Untitled'),
+            author=body.get('author', 'Anonymous'),
+            description=body.get('description', ''),
+            level_data=body.get('level_data', {}),
+            thumbnail=body.get('thumbnail', '')
+        )
         return JsonResponse({'id': p.id, 'title': p.title, 'status': 'ok'})
     return JsonResponse({'error': 'POST only'}, status=405)
 
@@ -26,7 +42,15 @@ def project_create(request):
 def project_detail(request, pk):
     try:
         p = Project.objects.get(pk=pk)
-        return JsonResponse({'id': p.id, 'title': p.title, 'author': p.author, 'description': p.description, 'level_data': p.level_data, 'likes': p.likes, 'plays': p.plays, 'thumbnail': p.thumbnail})
+        return JsonResponse({
+            'id': p.id,
+            'title': p.title,
+            'author': p.author,
+            'description': p.description,
+            'level_data': p.level_data,
+            'likes': p.likes,
+            'plays': p.plays,
+            'thumbnail': p.thumbnail
+        })
     except Project.DoesNotExist:
         return JsonResponse({'error': 'Not found'}, status=404)
-"@ | Out-File -Encoding utf8 server/community/views.py
