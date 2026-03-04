feec442d2d2a17d014f2d03e4259e05c2df3b417
Hack6297 <164305482+Hack6297@users.noreply.github.com>
2026-03-03 21:16:28 -0800

    Fix string representation method in Project model

--- server/community/models.py
+++ server/community/models.py
@@ -1,4 +1,3 @@
-@"
 from django.db import models
 
 class Project(models.Model):
@@ -17,4 +16,3 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
-"@ | Out-File -Encoding utf8 server/community/models.py
