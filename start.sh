5df670d29ab7985e8949e346db664a6ef81982b1
Hack6297 <164305482+Hack6297@users.noreply.github.com>
2026-03-03 21:18:42 -0800

    Fix syntax error in start.sh for daphne command

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-@"
 #!/usr/bin/env bash
 cd server
-daphne -b 0.0.0.0 -p `$PORT fne_backend.asgi:application
-"@ | Out-File -Encoding utf8 start.sh
+daphne -b 0.0.0.0 -p $PORT fne_backend.asgi:application
