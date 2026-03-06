09b9be7089f472a6190f29212c9b2bd985b91ecc
Hack6297 <164305482+Hack6297@users.noreply.github.com>
2026-03-05 17:00:38 -0800

    Update start.sh to set DJANGO_SETTINGS_MODULE

--- start.sh
+++ start.sh
@@ -1,3 +1,1 @@
-#!/usr/bin/env bash
-cd server
-daphne -b 0.0.0.0 -p $PORT fne_backend.asgi:application
+cd server && DJANGO_SETTINGS_MODULE=fne_backend.settings /opt/render/project/src/.venv/bin/daphne -b 0.0.0.0 -p ${PORT:-10000} fne_backend.asgi:application
